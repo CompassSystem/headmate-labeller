3ee473a5a2ea7f0ed8c41909e259b871f3f6f54b
Compass System <bucketofcompasses@gmail.com>
2023-11-20 13:20:23 +0000

    Fix birthday deserialisation.

--- src/main/kotlin/compass_system/headmate_labeller/PluralKitTypes.kt
+++ src/main/kotlin/compass_system/headmate_labeller/PluralKitTypes.kt
@@ -1,8 +1,14 @@
 package compass_system.headmate_labeller
 
-import kotlinx.datetime.Instant
+import kotlinx.datetime.*
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.lang.IllegalStateException
 
 @Serializable
@@ -22,6 +28,7 @@ data class PkMember(
 	@SerialName("display_name")
 	val displayName: String?,
 	val color: String?,
+	@Serializable(with = BirthdaySerializer::class)
 	val birthday: Instant?,
 	val pronouns: String?,
 	@SerialName("avatar_url")
@@ -30,6 +37,28 @@ data class PkMember(
 	@SerialName("proxy_tags")
 	val proxyTags: List<PkProxy>
 )
+
+class BirthdaySerializer : KSerializer<Instant> {
+	override val descriptor = PrimitiveSerialDescriptor("compass_system.headmate_labeller.BirthdaySerializer", PrimitiveKind.STRING)
+
+	override fun deserialize(decoder: Decoder): Instant {
+		val value = decoder.decodeString()
+
+		if (value.length == 4 + 3 + 3) {
+			val year = value.take(4).toInt()
+			val month = value.drop(5).take(2).toInt()
+			val day = value.takeLast(2).toInt()
+
+			return LocalDate(year, month, day).atTime(0, 0, 0, 0).toInstant(TimeZone.UTC)
+		}
+
+		throw SerializationException("Invalid birthday format.")
+	}
+
+	override fun serialize(encoder: Encoder, value: Instant) {
+		TODO("Serialisation not yet needed.")
+	}
+}
 
 @Serializable
 data class PkProxy(
