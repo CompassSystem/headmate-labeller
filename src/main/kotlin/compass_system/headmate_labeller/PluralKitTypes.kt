5f8c493b4345b87afc7b21478dc6958e4b7c71eb
Compass System <bucketofcompasses@gmail.com>
2023-11-21 11:58:21 +0000

    Fix PkProxy marking some valid proxies as invalid.

--- src/main/kotlin/compass_system/headmate_labeller/PluralKitTypes.kt
+++ src/main/kotlin/compass_system/headmate_labeller/PluralKitTypes.kt
@@ -66,18 +66,10 @@ data class PkProxy(
 	val suffix: String? = null
 ) {
 	override fun toString(): String {
-		if ((prefix != null && suffix != null) || (prefix == null && suffix == null)) {
-			throw IllegalStateException("Proxy must have either a prefix or a suffix, but not both.")
+		if (prefix == null && suffix == null) {
+			throw IllegalStateException("Proxy must at least a prefix or a suffix.")
 		}
 
-		if (prefix != null) {
-			return "${prefix}text"
-		}
-
-		if (suffix != null) {
-			return "text${suffix}"
-		}
-
-		throw IllegalStateException("Illegal flow control.")
+		return "${prefix ?: ""}text${suffix ?: ""}"
 	}
 }
